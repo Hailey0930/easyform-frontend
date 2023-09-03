18fb33805e9c9d5009d2d6806c62a8730c216cfc
Hailey0930 <hyerin0930@gmail.com>
2023-09-03 22:43:50 +0900

    Edit run build error

--- src/components/commons/layout/header/Header.tsx
+++ src/components/commons/layout/header/Header.tsx
@@ -81,91 +81,89 @@ export default function Header() {
     router.replace("/");
   };
 
-  return (
-    mounted && (
-      <div>
-        <S.PCTBWrapper>
-          <S.LogoContainer onClick={onClickLogo}>
-            <S.LogoImage src="/assets/header/logo.png" />
-          </S.LogoContainer>
-          <div>{userInfo?.email}</div>
+  return mounted ? (
+    <div>
+      <S.PCTBWrapper>
+        <S.LogoContainer onClick={onClickLogo}>
+          <S.LogoImage src="/assets/header/logo.png" />
+        </S.LogoContainer>
+        <div>{userInfo?.email}</div>
 
-          {login.isLogin ? (
-            <S.PCTBRightContainer>
-              <S.AlertSearchIcon>
-                <S.AlertSearchIconImage src="/assets/header/icon_alert.png" />
-              </S.AlertSearchIcon>
-              <S.ProfileContainer ref={outSide}>
-                <S.ProfileImage />
-                <S.ProfileButtonIcon
-                  onClick={onClickProfileButton}
-                  isDisplayProfile={isDisplayProfile}
-                ></S.ProfileButtonIcon>
-              </S.ProfileContainer>
-            </S.PCTBRightContainer>
+        {login.isLogin ? (
+          <S.PCTBRightContainer>
+            <S.AlertSearchIcon>
+              <S.AlertSearchIconImage src="/assets/header/icon_alert.png" />
+            </S.AlertSearchIcon>
+            <S.ProfileContainer ref={outSide}>
+              <S.ProfileImage />
+              <S.ProfileButtonIcon
+                onClick={onClickProfileButton}
+                isDisplayProfile={isDisplayProfile}
+              ></S.ProfileButtonIcon>
+            </S.ProfileContainer>
+          </S.PCTBRightContainer>
+        ) : (
+          <S.LoginButton onClick={onClickLoginButton}>Log In</S.LoginButton>
+        )}
+
+        <S.ProfileModal isDisplayProfile={isDisplayProfile}>
+          <S.ModalTitle>Account</S.ModalTitle>
+          <S.EmailWrapper onClick={onClickEmail}>
+            <S.ModalIcon>
+              <S.ModalIconImage src="/assets/header/icon_modal_email.png" />
+            </S.ModalIcon>
+            <S.EmailInfo>{userInfo?.email}</S.EmailInfo>
+          </S.EmailWrapper>
+          <S.BorderLine />
+          <S.LogoutWrapper onClick={onClickLogOut}>
+            <S.ModalIcon>
+              <S.ModalIconImage src="/assets/header/icon_modal_logout.png" />
+            </S.ModalIcon>
+            <S.Logout>Log out</S.Logout>
+          </S.LogoutWrapper>
+        </S.ProfileModal>
+      </S.PCTBWrapper>
+
+      <S.MBWrapper>
+        {!isDisplaySearch ? (
+          login.isLogin ? (
+            <>
+              <S.LogoContainer onClick={onClickLogo}>
+                <S.LogoImage src="/assets/header/logo.png" />
+              </S.LogoContainer>
+              <S.MBRightContainer>
+                <S.AlertSearchIcon onClick={onClickSearchIcon}>
+                  <S.AlertSearchIconImage src="/assets/header/icon_search.png" />
+                </S.AlertSearchIcon>
+                <S.HamburgerIcon>
+                  <S.HamburgerIconImage src="/assets/header/icon_hamburger.png" />
+                </S.HamburgerIcon>
+              </S.MBRightContainer>
+            </>
           ) : (
-            <S.LoginButton onClick={onClickLoginButton}>Log In</S.LoginButton>
-          )}
-
-          <S.ProfileModal isDisplayProfile={isDisplayProfile}>
-            <S.ModalTitle>Account</S.ModalTitle>
-            <S.EmailWrapper onClick={onClickEmail}>
-              <S.ModalIcon>
-                <S.ModalIconImage src="/assets/header/icon_modal_email.png" />
-              </S.ModalIcon>
-              <S.EmailInfo>{userInfo?.email}</S.EmailInfo>
-            </S.EmailWrapper>
-            <S.BorderLine />
-            <S.LogoutWrapper onClick={onClickLogOut}>
-              <S.ModalIcon>
-                <S.ModalIconImage src="/assets/header/icon_modal_logout.png" />
-              </S.ModalIcon>
-              <S.Logout>Log out</S.Logout>
-            </S.LogoutWrapper>
-          </S.ProfileModal>
-        </S.PCTBWrapper>
-
-        <S.MBWrapper>
-          {!isDisplaySearch ? (
-            login.isLogin ? (
-              <>
-                <S.LogoContainer onClick={onClickLogo}>
-                  <S.LogoImage src="/assets/header/logo.png" />
-                </S.LogoContainer>
-                <S.MBRightContainer>
-                  <S.AlertSearchIcon onClick={onClickSearchIcon}>
-                    <S.AlertSearchIconImage src="/assets/header/icon_search.png" />
-                  </S.AlertSearchIcon>
-                  <S.HamburgerIcon>
-                    <S.HamburgerIconImage src="/assets/header/icon_hamburger.png" />
-                  </S.HamburgerIcon>
-                </S.MBRightContainer>
-              </>
-            ) : (
-              <>
-                <S.LogoContainer onClick={onClickLogo}>
-                  <S.LogoImage src="/assets/header/logo.png" />
-                </S.LogoContainer>
-                <S.LoginButton onClick={onClickLoginButton}>
-                  Log In
-                </S.LoginButton>
-              </>
-            )
-          ) : (
-            <S.MBSearchContainer>
-              <S.BackIconContainer onClick={onClickBackIcon}>
-                <S.BackIconImage src="/assets/header/icon_back_mb.png" />
-              </S.BackIconContainer>
-              <S.SearchWrapper>
-                <S.SearchIcon>
-                  <S.SearchIconImage src="/assets/header/icon_search.png" />
-                </S.SearchIcon>
-                <S.SearchInput type="search" placeholder="Search" />
-              </S.SearchWrapper>
-            </S.MBSearchContainer>
-          )}
-        </S.MBWrapper>
-      </div>
-    )
+            <>
+              <S.LogoContainer onClick={onClickLogo}>
+                <S.LogoImage src="/assets/header/logo.png" />
+              </S.LogoContainer>
+              <S.LoginButton onClick={onClickLoginButton}>Log In</S.LoginButton>
+            </>
+          )
+        ) : (
+          <S.MBSearchContainer>
+            <S.BackIconContainer onClick={onClickBackIcon}>
+              <S.BackIconImage src="/assets/header/icon_back_mb.png" />
+            </S.BackIconContainer>
+            <S.SearchWrapper>
+              <S.SearchIcon>
+                <S.SearchIconImage src="/assets/header/icon_search.png" />
+              </S.SearchIcon>
+              <S.SearchInput type="search" placeholder="Search" />
+            </S.SearchWrapper>
+          </S.MBSearchContainer>
+        )}
+      </S.MBWrapper>
+    </div>
+  ) : (
+    <></>
   );
 }
